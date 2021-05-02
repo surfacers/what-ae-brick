54a72d8c532385103695951e6737d1a29756f730
bemayr <bernhard@mayr.io>
2021-05-02 09:13:56 +0200

    xstate example added

--- screens/ScanScreen.tsx
+++ screens/ScanScreen.tsx
@@ -1,19 +1,40 @@
-import { Camera } from 'expo-camera';
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Camera } from "expo-camera";
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import BarcodeMask from 'react-native-barcode-mask';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import BarcodeMask from "react-native-barcode-mask";
+import { createMachine } from "xstate";
+import { useMachine } from "@xstate/react";
+
+const machine = createMachine({
+  initial: "idle",
+  states: {
+    idle: {
+      tags: "idle",
+      on: {
+        DOWN: "holding",
+      },
+    },
+    holding: {
+      tags: "pressing",
+      on: {
+        UP: "idle",
+      },
+    },
+  },
+});
 
 export default function HistoryScreen() {
   const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const [state, send] = useMachine(machine);
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
@@ -26,11 +47,20 @@ export default function HistoryScreen() {
   return (
     <View style={styles.container}>
       <Camera style={styles.camera}>
-        <BarcodeMask height={200} width={200} lineAnimationDuration={1000}></BarcodeMask>
+        <BarcodeMask
+          height={200}
+          width={200}
+          lineAnimationDuration={1000}
+        ></BarcodeMask>
+          <Text style={styles.text}>{state.tags}</Text>
+        
         <View style={styles.buttonContainer}>
           <TouchableOpacity
-            style={styles.button}>
-              <View style={styles.shutterButton}></View>
+            style={styles.button}
+            onPressIn={() => send("DOWN")}
+            onPressOut={() => send("UP")}
+            >
+            <View style={styles.shutterButton}></View>
           </TouchableOpacity>
         </View>
       </Camera>
@@ -47,25 +77,25 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     margin: 20,
   },
   button: {
     flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+    alignSelf: "flex-end",
+    alignItems: "center",
   },
   text: {
     fontSize: 18,
-    color: 'white',
+    color: "white",
   },
   shutterButton: {
     width: 50,
     height: 50,
     borderRadius: 50,
-    backgroundColor: 'white'
-  }
+    backgroundColor: "white",
+  },
 });
