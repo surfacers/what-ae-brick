a37002738837180d51b1fd0d0121313b7f735499
Daniel Bauer <daniel.bauer.x@gmail.com>
2021-05-13 18:45:08 +0200

    Add History Page

---
+++ machines/data-fetch.machine.ts
@@ -0,0 +1,80 @@
+import { assign, createMachine } from 'xstate';
+
+export interface DataFetchContext<TData> {
+    data?: TData[];
+}
+
+export type DataFetchEvent<TData> =
+    | {
+        type: 'FETCH'
+    }
+    | {
+        type: 'RESOLVE',
+        data: TData[]
+    }
+    | {
+        type: 'REJECT'
+    }
+    | {
+        type: 'RETRY'
+    };
+
+export enum DataFetchTag {
+    idle = 'idle',
+    loading = 'loading',
+    success = 'success',
+    error = 'error'
+}
+
+export default function createDataFetchMachine<TData>() { return createMachine<
+    DataFetchContext<TData>,
+    DataFetchEvent<TData>
+>(
+    {
+        id: 'data-fetch',
+        initial: 'idle',
+        states: {
+            idle: {
+                tags: DataFetchTag.idle,
+                on: {
+                    FETCH: 'loading'
+                }
+            },
+            loading: {
+                tags: DataFetchTag.loading,
+                invoke: {
+                    src: 'fetchData',
+                    onDone: {
+                        target: 'success',
+                        actions: 'assignDataToContext'
+                    },
+                    onError: 'failure'
+                }
+            },
+            success: {
+                tags: DataFetchTag.success,
+                on: {
+                    RETRY: 'loading'
+                }
+            },
+            failure: {
+                tags: DataFetchTag.error,
+                on: {
+                    RETRY: 'loading'
+                }
+            }
+        }
+    },
+    {
+        services: {
+            fetchData: () => () => { },
+        },
+        actions: {
+            assignDataToContext: assign((context, event: any) => {
+                return {
+                    data: event.data,
+                };
+            })
+        }
+    }
+)};
--- screens/HistoryScreen.tsx
+++ screens/HistoryScreen.tsx
@@ -1,32 +1,137 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Image, FlatList, SafeAreaView, StatusBar, StyleSheet, ActivityIndicator, TouchableHighlight, Button } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
+import simpleDataFetchMachine, { DataFetchTag } from '../machines/data-fetch.machine';
+
 import { Text, View } from '../components/Themed';
+import { useMachine } from '@xstate/react';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function ScanScreen() {
-  return (
+interface HistoryItem {
+    id: string,
+    brickId: string,
+    brickName: string,
+    uri: string,
+    timeStamp: Date
+}
+
+type ListItemParams = {
+    item: HistoryItem,
+    separators: any
+}
+const ListItem = ({ item, separators }: ListItemParams) => (
+    <TouchableHighlight
+        key={item.id}
+        onPress={() => { console.log('pres') }}
+        onShowUnderlay={separators.highlight}
+        onHideUnderlay={separators.unhighlight}>
+        <View style={styles.item}>
+            <Image style={styles.itemImage} source={{ uri: item.uri }} />
+            <Text style={styles.title}>{item.brickName}</Text>
+            <Ionicons size={30} name="md-chevron-forward" style={{ flex: 1, textAlign: 'right', color: 'grey', fontSize: 20 }} />
+        </View>
+    </TouchableHighlight>
+);
+const EmptyList = () => (
     <View style={styles.container}>
-      <Text style={styles.title}>History</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ScanScreen.tsx" />
+        <Image style={styles.image} source={require('../assets/images/empty-history.png')} />
+        <Text style={styles.help}>No scanned bricks</Text>
     </View>
-  );
+);
+const ItemDivider = () => (<View style={styles.divider}></View>);
+
+const dataFetchMachine = simpleDataFetchMachine<HistoryItem>();
+// TODO: Load from SQLLite Database
+const loadData = () => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const data: HistoryItem[] = [...new Array(30)].map((_, i) => ({
+            id: `${i}`,
+            brickId: '3001',
+            brickName: `item ${i + 1}`,
+            uri: 'https://cdn.rebrickable.com/media/thumbs/parts/ldraw/13/3001.png/85x85p.png',
+            timeStamp: new Date()
+        }))
+
+        // resolve(data)
+        // resolve([])
+        reject('No internet connection')
+    }, 1000)
+});
+
+export default function HistoryScreen() {
+    const [state, send, _] = useMachine(dataFetchMachine, {
+        services: {
+            fetchData: loadData
+        }
+    });
+
+    if (state.hasTag(DataFetchTag.idle)) {
+        send({ type: 'FETCH' })
+    }
+
+    return <View style={styles.container}>{
+        state.hasTag(DataFetchTag.loading) && state.context.data == null
+            ? <ActivityIndicator size="large" color="#0000ff" />
+            :
+        state.hasTag(DataFetchTag.success) && (state.context.data?.length ?? 0) > 0
+            ? <FlatList
+                style={styles.list}
+                data={state.context.data ?? []}
+                renderItem={({ item, separators }) => ListItem({ item, separators })}
+                keyExtractor={item => item.id}
+                ItemSeparatorComponent={ItemDivider}
+                onRefresh={() => send({ type: 'RETRY' })}
+                refreshing={state.hasTag(DataFetchTag.loading)} />
+            :
+        state.hasTag(DataFetchTag.success)
+            ? EmptyList()
+            : <View>
+                <Text style={{ marginBottom: 8, color: 'red' }}>
+                    Error while loading
+                </Text>
+                <Button
+                    onPress={() => send({ type: 'RETRY' })}
+                    title="Retry" />
+            </View>
+        }
+    </View>
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    item: {
+        width: '100%',
+        padding: 10,
+        marginVertical: 0,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    divider: {
+        backgroundColor: '#C5C5C7',
+        height: 1,
+        marginLeft: 20
+    },
+    title: {
+        fontSize: 20,
+    },
+    image: {
+        width: 250,
+        height: 250
+    },
+    help: {
+        fontSize: 18,
+        color: 'grey'
+    },
+    list: {
+        width: '100%'
+    },
+    itemImage: {
+        width: 40,
+        height: 40,
+        marginRight: 10
+    }
 });
